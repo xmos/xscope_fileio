4070892efed5ebae6dacbee73e6e5e8be0b367fe
Michael Banther <michaelb@LT000633.xmos.local>
2022-06-27 17:25:23 +0100

    Update copyright date range

--- xscope_fileio/src/xscope_io_device.c
+++ xscope_fileio/src/xscope_io_device.c
@@ -1,4 +1,4 @@
-// Copyright (c) 2020, XMOS Ltd, All rights reserved
+// Copyright (c) 2020-2022, XMOS Ltd, All rights reserved
 #include "xscope_io_device.h"
 #include <xcore/chanend.h>
 #include <xcore/hwtimer.h>
