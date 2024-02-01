c15463601c9a08d612ab4f3e7b46dad76d771815
xalbertoisorna” <albertoisorna@xmos.com>
2024-02-01 16:55:28 +0000

    increasing timeout

--- tests/test_throughput.py
+++ tests/test_throughput.py
@@ -26,12 +26,12 @@ test_sizes_hw2 = [5, 10, 7] # in MB
 def fn_run_throughput(size_mb, adapter_id: str = None):
     # run xrun -l to see adapters
     subprocess.run(["xrun", "-l"], check=True)
-    
+
     # create tmp folder and random file
     tmpdir = Path(tempfile.mkdtemp(prefix='tmp_throughput_', dir=file_dir))
     ref = np.random.randint(256, size=(size_mb * 1024 * 1024)).astype(np.uint8)
     ref.tofile(tmpdir/"throughput_ref.bin")
-    
+   
     # get adapter id if none is passed
     if adapter_id is None:
         with xtagctl.acquire("XCORE-AI-EXPLORER", timeout=10) as xtag_id:
@@ -58,12 +58,12 @@ def run_throughput(test_size=10, timeout=30):
     pr.join(timeout=timeout)
     return_code = pr.exitcode
     pr.terminate()
-    assert return_code == 0, "ERROR: xscope_fileio process failed"
+    assert return_code == 0, f"ERROR: xscope_fileio process failed with exit code {return_code}"
     assert not pr.is_alive(), "ERROR: xscope_fileio process did not quit"
 
 @pytest.mark.parametrize("test_size", test_sizes_hw1)
 def test_throughput_1(test_size):
-    run_throughput(test_size, timeout=60)
+    run_throughput(test_size, timeout=80)
 
 @pytest.mark.parametrize("test_size", test_sizes_hw2)
 def test_throughput_2(test_size):
@@ -73,4 +73,4 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Run xscope_fileio_close.xe")
     parser.add_argument("--adapter-id", help="adapter_id to use", default=None)
     args = parser.parse_args()
-    fn_run_throughput(3, args.adapter_id)
+    fn_run_throughput(64, args.adapter_id)
