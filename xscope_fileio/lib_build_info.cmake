307585479a6a4cb9855236d45548ce554fb29a49
xalbertoisorna” <albertoisorna@xmos.com>
2024-11-22 13:34:20 +0000

    Add Python package installation for xscope-fileio in CMake configuration

--- xscope_fileio/lib_build_info.cmake
+++ xscope_fileio/lib_build_info.cmake
@@ -3,4 +3,10 @@ set(LIB_VERSION 1.3.1)
 set(LIB_INCLUDES api .)
 set(LIB_DEPENDENT_MODULES "")
 
+find_package(Python COMPONENTS Interpreter REQUIRED)
+if(DEFINED ENV{VIRTUAL_ENV} OR DEFINED ENV{CONDA_PREFIX})
+    execute_process(COMMAND ${Python_EXECUTABLE} -m pip install xscope-fileio==${LIB_VERSION})
+    message(STATUS "Installing xscope-fileio ${LIB_VERSION} using pip")
+endif()
+
 XMOS_REGISTER_MODULE()
