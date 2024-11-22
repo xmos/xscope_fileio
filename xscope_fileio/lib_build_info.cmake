21403b006e147476cb648b66eab2834c152db235
xalbertoisorna” <albertoisorna@xmos.com>
2024-11-22 13:42:13 +0000

    testing cmake install

--- xscope_fileio/lib_build_info.cmake
+++ xscope_fileio/lib_build_info.cmake
@@ -1,12 +1,24 @@
+function(install_if_not_installed lib_name lib_version)
+    find_package(Python COMPONENTS Interpreter REQUIRED)
+    if(DEFINED ENV{VIRTUAL_ENV} OR DEFINED ENV{CONDA_PREFIX})
+        execute_process(
+            COMMAND ${Python_EXECUTABLE} -c "import pkg_resources; pkg_resources.require('${lib_name}==${lib_version}')"
+            RESULT_VARIABLE pkg_check_result
+            OUTPUT_QUIET
+            ERROR_QUIET
+        )
+        if(NOT pkg_check_result EQUAL 0)
+            execute_process(COMMAND ${Python_EXECUTABLE} -m pip install ${lib_name}==${lib_version})
+            message(STATUS "Installing ${lib_name} ${lib_version} using pip")
+        else()
+            message(STATUS "${lib_name} ${lib_version} is already installed")
+        endif()
+    endif()
+endfunction()
+
 set(LIB_NAME xscope_fileio)
 set(LIB_VERSION 1.3.1)
 set(LIB_INCLUDES api .)
 set(LIB_DEPENDENT_MODULES "")
-
-find_package(Python COMPONENTS Interpreter REQUIRED)
-if(DEFINED ENV{VIRTUAL_ENV} OR DEFINED ENV{CONDA_PREFIX})
-    execute_process(COMMAND ${Python_EXECUTABLE} -m pip install xscope-fileio==${LIB_VERSION})
-    message(STATUS "Installing xscope-fileio ${LIB_VERSION} using pip")
-endif()
-
+install_if_not_installed(${LIB_NAME} ${LIB_VERSION})
 XMOS_REGISTER_MODULE()
