5d0adb05d5f73f32f0afe5a313a3273aacc84ae2
xalbertoisorna” <albertoisorna@xmos.com>
2024-11-22 13:55:55 +0000

    Rename function to get_python_package and update installation logic to provide warnings for already installed packages

--- xscope_fileio/lib_build_info.cmake
+++ xscope_fileio/lib_build_info.cmake
@@ -1,24 +1,24 @@
-function(install_if_not_installed lib_name lib_version)
+function(get_python_package lib_name lib_version)
     find_package(Python COMPONENTS Interpreter REQUIRED)
-    if(DEFINED ENV{VIRTUAL_ENV} OR DEFINED ENV{CONDA_PREFIX})
+    if(DEFINED ENV{VIRTUAL_ENV} OR DEFINED ENV{CONDA_PREFIX}) # only if you have a venv or conda env active
         execute_process(
             COMMAND ${Python_EXECUTABLE} -c "import pkg_resources; pkg_resources.require('${lib_name}==${lib_version}')"
             RESULT_VARIABLE pkg_check_result
             OUTPUT_QUIET
             ERROR_QUIET
         )
-        if(NOT pkg_check_result EQUAL 0)
+        if(NOT pkg_check_result EQUAL 0) # if the package is not installed already or version is wrong, install
             execute_process(COMMAND ${Python_EXECUTABLE} -m pip install ${lib_name}==${lib_version} COMMAND_ERROR_IS_FATAL ANY)
             message(STATUS "Installing ${lib_name} ${lib_version} using pip")
-        else()
-            message(STATUS "${lib_name} ${lib_version} is already installed")
+        else() # else, just warn the user
+            message(WARNING "${lib_name} ${lib_version} is already installed")
         endif()
     endif()
 endfunction()
 
 set(LIB_NAME xscope_fileio)
-set(LIB_VERSION 1.3.0)
+set(LIB_VERSION 1.3.0) #
 set(LIB_INCLUDES api .)
 set(LIB_DEPENDENT_MODULES "")
-install_if_not_installed(${LIB_NAME} ${LIB_VERSION})
+get_python_package(${LIB_NAME} ${LIB_VERSION})
 XMOS_REGISTER_MODULE()
