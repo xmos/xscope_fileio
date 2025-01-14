84e1dcccb9b5af8290500945388f294fadabb492
xalbertoisorna” <albertoisorna@xmos.com>
2025-01-14 10:11:03 +0000

    Refactor test_simple function to directly run the target binary and simplify process handling

--- tests/test_simple.py
+++ tests/test_simple.py
@@ -11,26 +11,13 @@ import xscope_fileio
 test_path = Path(__file__).parent
 firmware_xe = test_path / "simple" / "bin" / "test_simple.xe"
 
-
-def fn_simple(adapter_id: str = None):
-    print("Firmware: ", firmware_xe)
-    use_xsim = True if adapter_id is None else False
-    rtrn_code = xscope_fileio.run_on_target(adapter_id, firmware_xe, use_xsim=use_xsim)
-    assert rtrn_code == 0, "xscope_fileio.run_on_target() failed"
-
-
 def test_simple(adapter_id: str = None):
     """
-    This function is just a wrapper to control
-    time of execution of the close files test in case of hang
+    This function runs test simple binary on the target.
     """
-    pr = Process(target=fn_simple, args=(adapter_id,))
-    pr.start()
-    pr.join(timeout=30)
-    return_code = pr.exitcode
-    pr.terminate()
+    use_xsim = True if adapter_id is None else False
+    return_code = xscope_fileio.run_on_target(adapter_id, firmware_xe, use_xsim=use_xsim)
     assert return_code == 0, "ERROR: test_simple failed"
-    assert not pr.is_alive(), "ERROR: xscope_fileio process did not quit"
     time.sleep(10) # give 10 seconds to xsim to close
 
 
@@ -38,4 +25,4 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Run xscope_fileio_close.xe")
     parser.add_argument("--adapter-id", help="adapter_id to use", default=None)
     args = parser.parse_args()
-    fn_simple(args.adapter_id)
+    test_simple(args.adapter_id)
